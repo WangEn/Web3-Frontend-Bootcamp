6d863c745c7f4bc12ec602723e650e93b765b0b0
Warn <imwarn@163.com>
2024-06-06 12:15:16 +0800

    fix: localStorage存取

--- practice/src/App.tsx
+++ practice/src/App.tsx
@@ -1,8 +1,8 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import Header from './components/Header'
-import AddTodo from './components/AddToDo'
-import TodoList from './components/TodoList'
+import { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import AddTodo from "./components/AddToDo";
+import TodoList from "./components/TodoList";
 interface Todo {
   id: number;
   text: string;
@@ -10,47 +10,41 @@ interface Todo {
 }
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([])
+  const [todos, setTodos] = useState<Todo[]>(
+    localStorage.getItem("todos")
+      ? JSON.parse(localStorage.getItem("todos")!)
+      : []
+  );
 
   useEffect(() => {
-    const savedTodos = localStorage.getItem('todos')
-    console.log(savedTodos)
-    if (savedTodos) {
-      // setTodos(JSON.parse(savedTodos))
-      try {
-        setTodos(JSON.parse(savedTodos));
-      } catch (e) {
-        console.error('Failed to parse todos from localStorage', e);
-      }
-    }
-  }, [])
-
-  useEffect(() => {
-    console.log(todos)
-    console.log(localStorage.getItem("todos"))
-    localStorage.setItem('todos', JSON.stringify(todos))
-  }, [todos])
+    console.log(todos);
+    console.log(localStorage.getItem("todos"));
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   const addTodo = (text: string) => {
-    setTodos([...todos, { id: Date.now(), text, completed: false }])
-  }
+    setTodos([...todos, { id: Date.now(), text, completed: false }]);
+  };
 
   const toggleTodo = (id: number) => {
-    setTodos(todos.map(todo => todo.id === id ? { ...todo, completed: !todo.completed } : todo))
-  }
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
 
   const deleteTodo = (id: number) => {
-    setTodos(todos.filter(todo => todo.id !== id))
-  }
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
 
   return (
     <>
       <Header />
       <AddTodo onAdd={addTodo} />
       <TodoList todos={todos} onToggle={toggleTodo} onDelete={deleteTodo} />
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
